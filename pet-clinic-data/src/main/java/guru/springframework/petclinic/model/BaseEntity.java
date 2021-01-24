546acdcc6f41772927a7de95e21e5ca1c181b047
Onkar <onkar.vaidya.49@gmail.com>
2021-01-24 12:37:20 +0530

    Adding BaseEntity class and extending other model classes with from BaseEntity

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/BaseEntity.java
@@ -0,0 +1,16 @@
+package guru.springframework.petclinic.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+    private Long Id;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+}
